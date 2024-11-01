a6e7383cfd94e9e230a3cdc369a7bf7bcdce683f
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2024-11-02 08:28:29 +0900

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  site: "https://withmtml.github.io",
+  base: "/",
+  trailingSlash: "always",
+});
